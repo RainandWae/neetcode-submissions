40dda822875274dee520c5a077d6f295c6b493ca
Radium <35959143+RainandWae@users.noreply.github.com>
2026-07-13 09:32:38 +0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,20 @@
+#Iterative BINARY SEARCH
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            # find middle
+            # (l + r) // 2 can lead to overflow
+            m = l + ((r - l) // 2)
+
+            # binary search thing
+            # move middle pointer toward target 
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
