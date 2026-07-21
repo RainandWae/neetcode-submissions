ba02f438f71ff07f2a516c700cbf559e5d54ff0d
Radium <35959143+RainandWae@users.noreply.github.com>
2026-07-21 10:48:33 +0700

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# dfs, go all way down, carry max value seen so far
+# if current node value >= max value, good node
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            result = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            result += dfs(node.left, maxVal)
+            result += dfs(node.right, maxVal)
+            return result
+
+        return dfs(root, root.val)
